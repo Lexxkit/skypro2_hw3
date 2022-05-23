a438c3be25f46e90775bfbd9fa1a4a4f7d3acff3
Lexxkit <lexxkit@gmail.com>
2022-05-23 17:33:14 +0300

    Original code with mistakes

---
+++ src/com/lexxkit/Bicycle.java
@@ -0,0 +1,10 @@
+package com.lexxkit;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
